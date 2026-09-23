7e00de1aa8b9fe5b45c3fe45284e3656221cbddc
Fabrizio Mastrogiovanni <fabriziocontact1@gmail.com>
2026-09-23 14:06:32 -0400

    changing the deployment reference

--- remediate_ai.py
+++ remediate_ai.py
@@ -10,7 +10,7 @@ repo = os.getenv("GITHUB_REPOSITORY")
 pr_number = os.getenv("PR_NUMBER")
 
 # YOUR DEPLOYMENT NAME
-DEPLOY_NAME = "dso-ai-001"
+DEPLOY_NAME = "Agent742"
 
 
 def get_ai_fix(issue):
